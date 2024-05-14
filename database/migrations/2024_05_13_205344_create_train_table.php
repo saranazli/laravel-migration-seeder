b8fcf0096159abe4b2876a031019de131e45d990
s4r44d43 <sararez@live.it>
2024-05-14 17:54:15 +0200

    [...]

--- database/migrations/2024_05_13_205344_create_train_table.php
+++ database/migrations/2024_05_13_205344_create_train_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('train', function (Blueprint $table) {
+        Schema::create('trains', function (Blueprint $table) {
             $table->id();
             $table->string('agency', 100);
             $table->string('leaving_station', 100);
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('train');
+        Schema::dropIfExists('trains');
     }
 };
--- database/migrations/2024_05_13_211854_update_train_table.php
+++ database/migrations/2024_05_13_211854_update_train_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('train', function (Blueprint $table) {
+        Schema::table('trains', function (Blueprint $table) {
             $table->string('brand')->after('deleted');
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('train', function (Blueprint $table) {
+        Schema::table('trains', function (Blueprint $table) {
             $table->dropColumn('brand');
         });
     }
